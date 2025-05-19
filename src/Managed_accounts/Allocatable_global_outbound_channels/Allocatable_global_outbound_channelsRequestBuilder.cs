665284aec06d8929f45df3ed9d0dc024446d66cf
Jake Soenneker <jake@soenneker.com>
2025-05-19 17:09:19 +0000

    Automated update

--- src/Managed_accounts/Allocatable_global_outbound_channels/Allocatable_global_outbound_channelsRequestBuilder.cs
+++ src/Managed_accounts/Allocatable_global_outbound_channels/Allocatable_global_outbound_channelsRequestBuilder.cs
@@ -3,7 +3,6 @@
 using Microsoft.Kiota.Abstractions.Extensions;
 using Microsoft.Kiota.Abstractions.Serialization;
 using Microsoft.Kiota.Abstractions;
-using Soenneker.Telnyx.OpenApiClient.Models;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -39,7 +38,6 @@ namespace Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_out
         /// <returns>A <see cref="global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsGetResponse"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
-        /// <exception cref="global::Soenneker.Telnyx.OpenApiClient.Models.Errors">When receiving a 403 status code</exception>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
         public async Task<global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsGetResponse?> GetAsAllocatable_global_outbound_channelsGetResponseAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
@@ -50,11 +48,7 @@ namespace Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_out
         {
 #endif
             var requestInfo = ToGetRequestInformation(requestConfiguration);
-            var errorMapping = new Dictionary<string, ParsableFactory<IParsable>>
-            {
-                { "403", global::Soenneker.Telnyx.OpenApiClient.Models.Errors.CreateFromDiscriminatorValue },
-            };
-            return await RequestAdapter.SendAsync<global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsGetResponse>(requestInfo, global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsGetResponse.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
+            return await RequestAdapter.SendAsync<global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsGetResponse>(requestInfo, global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsGetResponse.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Display information about allocatable global outbound channels for the current user. Only usable by account managers.
@@ -62,7 +56,6 @@ namespace Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_out
         /// <returns>A <see cref="global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsResponse"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
-        /// <exception cref="global::Soenneker.Telnyx.OpenApiClient.Models.Errors">When receiving a 403 status code</exception>
         [Obsolete("This method is obsolete. Use GetAsAllocatable_global_outbound_channelsGetResponseAsync instead.")]
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -74,11 +67,7 @@ namespace Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_out
         {
 #endif
             var requestInfo = ToGetRequestInformation(requestConfiguration);
-            var errorMapping = new Dictionary<string, ParsableFactory<IParsable>>
-            {
-                { "403", global::Soenneker.Telnyx.OpenApiClient.Models.Errors.CreateFromDiscriminatorValue },
-            };
-            return await RequestAdapter.SendAsync<global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsResponse>(requestInfo, global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsResponse.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
+            return await RequestAdapter.SendAsync<global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsResponse>(requestInfo, global::Soenneker.Telnyx.OpenApiClient.Managed_accounts.Allocatable_global_outbound_channels.Allocatable_global_outbound_channelsResponse.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Display information about allocatable global outbound channels for the current user. Only usable by account managers.
--- src/Models/RecordType.cs
+++ src/Models/RecordType.cs
@@ -3,13 +3,13 @@ using System.Runtime.Serialization;
 using System;
 namespace Soenneker.Telnyx.OpenApiClient.Models
 {
-    /// <summary>Identifies the type of the resource.</summary>
+    /// <summary>Identifies record type.</summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public enum RecordType
     {
-        [EnumMember(Value = "event")]
+        [EnumMember(Value = "custom_storage_credentials")]
         #pragma warning disable CS1591
-        Event,
+        Custom_storage_credentials,
         #pragma warning restore CS1591
     }
 }
